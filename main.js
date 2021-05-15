c806ebf860c5c365266d08a93b9177285b623825
Vsevolod <cerneavschivsevolod@gmail.com>
2021-05-15 14:04:23 +0200

    Modifiche aggiuntive

--- main.js
+++ main.js
@@ -2,30 +2,39 @@
 // Rendiamo il tutto gradevole alla vista.
 // Permettiamo la modifica di un todo già aggiunto
 
-var btn_aggiungi = $('button[name="add"]');
-var listaCompiti = $('div.compiti-reali');
-
-
-btn_aggiungi.click(function() {
-  var inputValue = $('input[name="input"]').val();
-  if (inputValue) {
-    var nuovaRiga = $('.template .flex').clone();
-    listaCompiti.append(nuovaRiga)
-    nuovaRiga.find('div.rowDo').append(inputValue);
-    $('input[name="input"]').val('');
-  } else {
-    alert("Il compito non può essere vuoto!")
-  }
+$('button[name=add]').click(function() {
+  aggiunta();
 })
+
+$('input').keypress(function(e) {
+  if (e.which == 13) {
+    aggiunta();
+  }
+});
+
+
+function aggiunta() {
+
+  var inputTxt = $('input').val();
+  if (inputTxt == '') {
+    alert('input non puo essere vuoto')
+  } else {
+    var templateClone = $('.template .flex').clone();
+    var appendToCLone = templateClone.children('.rowDo').text(inputTxt);
+    $('.compiti-reali').append(templateClone);
+    $('input').val('');
+  }
+
+}
 
 
 var btn_elimina = $('button[name="canc"]');
 
 btn_elimina.click(function() {
   var checkbox_collection = $('input[type="checkbox"]');
-  checkbox_collection.each(function(){
-    if($(this).prop('checked')) {
-    $(this).parents('.flex').remove();
+  checkbox_collection.each(function() {
+    if ($(this).prop('checked')) {
+      $(this).parents('.flex').remove();
     }
   })
 })
@@ -35,17 +44,20 @@ btn_elimina.click(function() {
 var btn_modifica = $('button[name="edit"]');
 
 btn_modifica.click(function() {
-var text_da_modificare = $("input:checked").parent().siblings('.rowDo').text();
-  $("input:checked").parent().siblings('.rowDo').text('');
-  $("input:checked").parent().parent().parent().siblings('.aggiungi').find('input[name="input"]').val($.trim(text_da_modificare));
+  var text_da_modificare = $('input[type="radio"]:checked').parent().siblings('.rowDo').text();
+  $('input[type="radio"]:checked').parent().siblings('.rowDo').text('');
+  // $("input:checked").parent().parent().parent().siblings('.aggiungi').find('input[name="input"]').val($.trim(text_da_modificare));
+  $('input[type="text"]').val(text_da_modificare);
 });
 
 // -----------------------------------------------
 
 var btn_modifica_completata = $('button[name="finish_edit"]');
 
-btn_modifica_completata.click(function(){
-  var inputBackChecked = $("input:checked");
-  var valueToBack = $('.aggiungi input[name="input"]').val();
-  inputBackChecked.append(valueToBack);
+btn_modifica_completata.click(function() {
+  var radioChecked = $('input[type="radio"]:checked');
+  var inputTxt = $('input').val();
+  var rowDoChecked = radioChecked.parent().siblings('.rowDo');
+  rowDoChecked.text(inputTxt);
+
 })
